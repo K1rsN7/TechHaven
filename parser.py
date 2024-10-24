6920bdc3a6ce2259115bd00b0fccfa00a56b21f2
K1rsN7 <k1rsn7@mail.ru>
2024-10-24 12:28:07 +0300

    Добавления полученных данных в базу данных

--- parser.py
+++ parser.py
@@ -4,14 +4,27 @@ import json
 import os
 import g4f
 from tqdm import tqdm
+import pandas as pd
+from sqlalchemy import create_engine
 
-#dict_category = {'Компьютеры': 1, 'Процессоры': 2, 'Видеокарты': 3, 'Материнские платы': 4, 'Оперативная память': 5, 'Диски SSD': 6, 'Диски HDD': 7, 'Блоки питания': 8, 'Корпуса': 9, 'Кулеры': 10, 'Новинки': 11, 'Мониторы': 12, 'Аксессуары': 13, 'Клавиатуры и мыши': 14}
-dict_category = {}
+dict_category = {} # Словарь категорий
+
 # Загрузка конфигурации из JSON файла
 with open('pars_keys.json', 'r') as file:
     list_urls_and_keys = json.load(file)["list_urls_and_keys"]
 
-def fetch_data(url, element, key):
+def fetch_data(url:str, element:str, key:str) -> tuple[int, list[str]]:
+    """Получение текста, который содержит конкретный элемент на странице
+
+    Args:
+        url (str): ссылка на сайт
+        element (str): элемент HTML кода
+        key (str): класс элемента HTML кода
+
+    Returns:
+        count (int): количество найденных элементов на странице
+        items (list[str]): текст найденного элемента
+    """
     try:
         response = requests.get(url)
         response.raise_for_status()  # Генерирует исключение для ошибок HTTP
@@ -31,18 +44,9 @@ def fetch_data(url, element, key):
     # Для других элементов возвращаем текст
     return len(items), [item.get_text(strip=True) for item in items]
 
-def save_results(name, results):
-    # Создание директории для сохранения результатов, если она не существует
-    os.makedirs('results', exist_ok=True)
-    
-    # Формирование имени файла
-    file_path = f'results/{name}.json'
-    
-    # Запись результатов в JSON файл
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(results, f, ensure_ascii=False, indent=4)
-
 def parse_data():
+    """Получение информации с сайта в соответствии с ключами файла pars_keys.json
+    """
     if not os.path.exists("./data"):
         os.makedirs("./data")
     for item in list_urls_and_keys:
@@ -59,6 +63,7 @@ def parse_data():
                 
                 print(f"======Данные для {name}: {url}======")
                 
+                # Продукция размещается не на одной странице
                 if is_pages:
                     for page in range(1, 25):
                         current_url = f"{url}?page={page}" if page != 1 else url
@@ -74,12 +79,23 @@ def parse_data():
                     all_results[field] = results  # Добавляем результаты в общий список
                     break  # Выход из цикла после первого запроса
         
+        # Сохранение результатов в JSON файл
         if not os.path.exists(f"./data/{name}.json"):
             open(f"./data/{name}.json", 'a').close()
         with open(f"./data/{name}.json", 'w', encoding='utf-8') as json_file:
             json.dump(all_results, json_file, ensure_ascii=False, indent=4)
 
-def characterizations_preprocessing(data):
+def characterizations_preprocessing(data: list[str]) -> tuple[list[str], list[str], list[str]]:
+    """Обработка сплошного массива данных содержащие внутри себя информацию об количестве дней доставки продукта, его бренде и гарантийным сроком
+
+    Args:
+        data (list[str]): список данных с количеством дней доставок, брендом и гарантийным сроком
+
+    Returns:
+        delivery_days (list[str]): список с количеством дней необходимых на доставку
+        brands (list[str]): список брендов продуктов
+        guarantee_month (list[str]): список количества месяцев гарантийного срока продукции
+    """
     delivery_days = []
     brands = []
     guarantee_months = []
@@ -100,30 +116,40 @@ def characterizations_preprocessing(data):
             i += 2
     return delivery_days, brands, guarantee_months
 
-def generate_product_description(product_name):
-    # Формируем сообщение для модели
+def generate_product_description(product_name:str) -> str:
+    """Генерация описания к продукту с использованием GPT-4o 
+
+    Args:
+        product_name (str): название продукта
+
+    Returns:
+        responce (str): _текст описания продукта
+    """
+    # Формирование запроса
     messages = [
         {"role": "user", "content": f"Сгенерируй описание для товара: {product_name}."}
     ]
     
-    # Запрос к модели
+    # Выполнение запроса и получение ответа
     response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_4o,  # Используем модель GPT-4
+        model=g4f.models.gpt_4o,  # Используем модель GPT-4o
         messages=messages
     )
     return response
 
-
-def data_processing_first_step():
+def process_category_and_product_data() -> None:
+    """Разбиение общей информации на отдельные записи
+    """
+    # Чтение всех файлов 
     for file in os.listdir("./data"):
         if file.endswith(".json"):
             with open(f"./data/{file}", 'r', encoding='utf-8') as f:
                 data = json.load(f)
-            
-            # Обработка категорий
             try:
+                # Обработка категорий
                 if len(data.keys()) == 1:
                     print(f"Обработка файла {file}...")
+                    
                     # Изменяем данные
                     data = data["category"]
                     if "Гарнитуры" in data:
@@ -131,17 +157,21 @@ def data_processing_first_step():
                         rows = [{"name_category": name_category} for name_category in data]
                         for id_category, name_category in enumerate(data, start=1):
                             dict_category[str(name_category)] = id_category
+                        
                         # Запись изменений обратно в файл
                         with open(f"./data/{file}", 'w', encoding='utf-8') as f:
                             json.dump(rows, f, ensure_ascii=False, indent=4)
                 else:
+                    # Обработка продуктов 
                     if len(data.keys()) == 4:
                         print(f"Обработка файла {file}...")
-                        images = [item for item in data["image_product"] if  len(item.split('/')[-1].split('.')) == 3]
-                        name_product = data["name_product"]
+                        images = [item for item in data["image_product"] if  len(item.split('/')[-1].split('.')) == 3] 
+                        name_product = data["name_product"] 
                         characterizations = data["characterizations"]
                         delivery_days, id_brands, guarantee_months = characterizations_preprocessing(characterizations)
                         price_product = data["price_product"]
+                        
+                        # Исправление
                         all_rows = []
                         for item in tqdm(range(0, len(name_product)), desc=f"Обработка данных файла {file}"):
                             rows = {}
@@ -159,9 +189,12 @@ def data_processing_first_step():
             except:
                 pass
 
-def data_processing_second_step():
-    all_brands = []
-    print("Получение всех брендов")
+def process_brands_in_records() -> None:
+    """Обработка брендов в записях
+    """
+    all_brands = [] # Список всех брендов продуктов
+    
+    # Получение всех брендом продуктов
     for file in os.listdir("./data"):
         if file.endswith(".json"):
             with open(f"./data/{file}", 'r', encoding='utf-8') as f:
@@ -169,12 +202,12 @@ def data_processing_second_step():
                 if "id_brand" in data[0].keys():
                     for row in data:
                         all_brands.append(row["id_brand"])
-    print("Формирование словаря брендов")
     all_brands = set(all_brands)
+    
+    # Создание словаря для индексирования брендов
     brand_dict = {}
     for id_brand, name_brand in enumerate(all_brands, start=1):
         brand_dict[str(name_brand)] = id_brand
-
     key_old = None
     key_value = None
     for key, value in brand_dict.items():
@@ -183,10 +216,10 @@ def data_processing_second_step():
         
         if key == "None":
             key_value = value
-    
     brand_dict["None"] = 1
     brand_dict[key_old] = key_value
     
+    # Изменение всех названий бренда на индексы в записях
     for file in os.listdir("./data"):
         if file.endswith(".json"):
             with open(f"./data/{file}", 'r', encoding='utf-8') as f:
@@ -204,31 +237,53 @@ def data_processing_second_step():
                         with open(f"./data/{file}", 'w', encoding='utf-8') as json_file:
                             json.dump(all_rows, json_file, ensure_ascii=False, indent=4)
     
+    # Формирование файла для последующего импорта брендов в базу данных
     brand_keys = sorted(brand_dict, key=brand_dict.get)
     brand_data = []
     for key in brand_keys:
         brand_data.append({"name_brand": key})
-
     with open(f"./data/brand.json", 'w', encoding='utf-8') as json_file:
         json.dump(brand_data, json_file, ensure_ascii=False, indent=4)
-    
     print("Добавлен файл brand.json")
 
-def data_processing_third_step():
-    files = [file for file in os.listdir("./data") if file not in ['brand.json', 'category.json']]
-    all_data = []
-    for file in files:
-        with open(f"./data/{file}", 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            all_data.extend(data)
-    
-    with open(f"./data/product.json", 'w', encoding='utf-8') as json_file:
-        json.dump(all_data, json_file, ensure_ascii=False, indent=4)
-    
-    print("Добавлен файл product.json")
-    
+def import_data_in_database() -> None:
+    """Внесение всех записей из файлов директории data в базу данных
+    """
+    # Получение данных для подключения к базе данных
+    with open("config.json", "r") as f:
+        data = json.load(f)
+        db_username = data["DB_USER"]
+        db_password = data["DB_PASSWORD"]
+        hostname = f"{data["DB_HOST"]}:{data["DB_PORT"]}"
+        database_name = data["DB_NAME"]
+
+    # Настройка подключения к базе данных
+    engine = create_engine(f"mysql+pymysql://{db_username}:{db_password}@{hostname}/{database_name}")
+
+    # Импорт данных из CSV файлов в соответствующие таблицы
+    def import_data_from_json(file_path:str, table_name:str) -> None:
+        """Чтение данных из файла и последующий импорт их
+
+        Args:
+            file_path (str): путь до файла
+            table_name (str): название таблицы в базе данных
+        """
+        
+        df = pd.read_json(file_path)
+        df.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+    # Добавление всех записей в базу данных
+    for file in sorted(os.listdir("./data")):
+        if file.startswith("product_"):
+            import_data_from_json(f"./data/{file}", "product")
+        elif file.startswith("brand"):
+            import_data_from_json(f"./data/{file}", "brand")
+        elif file.startswith("category"):
+            import_data_from_json(f"./data/{file}", "category")
+        print(f"Импортированы данные из {file[:-5]} в базу данных")
+
 if __name__ == "__main__":
     parse_data()
-    data_processing_first_step()
-    data_processing_second_step()
-    data_processing_third_step()
+    process_category_and_product_data()
+    process_brands_in_records()
+    import_data_in_database()
